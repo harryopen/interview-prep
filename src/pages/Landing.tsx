cf3b12c312d9047fc7e7d067429f9d0e0a0d93f5
Harpreet singh <harpreetsingh@Harpreets-MacBook-Air.local>
2025-09-29 21:06:13 +0530

    initial setup

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import ContentDisplay from "./component/ContentDisplay";
+import TopicList from "./component/TopicList";
+import { topics } from "../assets/data";
+import { topicContent } from "../assets/data";
+
+const Landing = () => {
+  const [selectedTopic, setSelectedTopic] =
+    useState<string>(`react-fundamentals`);
+  const currentContent = topicContent[selectedTopic] || null;
+
+  const handleTopicSelect = (TopicId: string) => {
+    setSelectedTopic(TopicId);
+  };
+  return (
+    <div className="h-screen flex bg-background">
+      {" "}
+      <TopicList
+        topics={topics}
+        setSelectedTopic={setSelectedTopic}
+        onTopicSelect={handleTopicSelect}
+      />
+      <ContentDisplay content={currentContent} />
+    </div>
+  );
+};
+
+export default Landing;
