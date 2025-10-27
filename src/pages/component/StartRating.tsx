73039f51c9b9df12ac2cfdba0c6382b1c11f98dc
Harpreet singh <harpreetsingh@Harpreets-MacBook-Air.local>
2025-10-27 18:03:48 +0530

    added star rating question

--- src/pages/component/StartRating.tsx
+++ src/pages/component/StartRating.tsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
 const StartRating = () => {
-  return (
-    <div>StartRating</div>
-  )
-}
+  const [rating, setRating] = useState<number>(0);
+  const [hover, setHover] = useState<number>(0);
+  // local handlers
+  const handleGetIndex = (getIndex: number) => {
+    setRating(getIndex);
+  };
+  const handleMouseEnter = (getIndex: number) => {
+    setHover(getIndex);
+  };
+  const handleMouseLeave = () => {
+    setHover(rating);
+  };
 
-export default StartRating
+  console.log("log hover ", hover);
+  return (
+    <div className="flex ">
+      {[...Array(5)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            className={`${
+              index <= (hover || rating) ? "bg-red-500" : "bg-gray-300"
+            } w-6 h-6 rounded-full`}
+            onClick={() => handleGetIndex(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+            key={index}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default StartRating;
